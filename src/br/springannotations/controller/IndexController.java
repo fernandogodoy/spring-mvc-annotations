7e4b93a06ea6beeead173c65903b61ad7eb676cd
Fernando Godóy <fernandogodoy_18@msn.com>
2016-03-13 20:37:20 -0300

    CRIAÇAO PAGINA INDEX

--- src/br/springannotations/controller/IndexController.java
+++ src/br/springannotations/controller/IndexController.java
@@ -3,7 +3,7 @@ package br.springannotations.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller()
+@Controller
 @RequestMapping("/index")
 public class IndexController {
 
