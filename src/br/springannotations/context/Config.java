fc521d00c909e30d187508d088587e9878c6f018
Fernando Godóy <fernandogodoy_18@msn.com>
2016-03-13 20:16:09 -0300

    DECLARAÇÃO BEAN PARA RESOLVER PAGINAS

--- src/br/springannotations/context/Config.java
+++ src/br/springannotations/context/Config.java
@@ -1,8 +1,21 @@
 package br.springannotations.context;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
+@Component(value = "br.springannotations.*")
 public class Config {
 
+	@Bean
+	public InternalResourceViewResolver internalResourceViewResolver() {
+	  InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+	  resolver.setPrefix("/WEB-INF/views/");
+	  resolver.setSuffix(".jsp");
+	  return resolver;
+	}
+	
+	
 }
